a779965caf3eccad784c82f4ccf2aa0fa83f54a2
Andrew Newdigate <andrew@troupe.co>
2015-10-14 11:24:46 +0100

    Added rate-limit protection

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 /* jshint node:true */
 'use strict';
 
+var rateLimitDown = false;
+
 var Tentacles = require('tentacles');
-var tentacles = new Tentacles({ accessToken: process.env.GITHUB_TOKEN });
+var tentacles = new Tentacles({
+  accessToken: process.env.GITHUB_TOKEN,
+  extensions: [
+    function(options, callback, next) {
+       /* Call the next middleware in the chain */
+       next(options, function(err, response, body) {
+         var remaining = parseInt(response.headers['x-ratelimit-remaining'], 10);
+         if (remaining < 1500) {
+           rateLimitDown = true;
+         }
+         callback(err, response, body);
+       });
+    }
+  ]
+});
+
 var Q = require('q');
 var qlimit = require('qlimit');
 var limit = qlimit(2); // 2 being the maximum concurrency
@@ -42,6 +59,10 @@ function getAllForkedRepos(page) {
 
     })
     .then(function() {
+      if (rateLimitDown) {
+        console.log('Stopping for now as rate limit is running out...');
+        return;
+      }
       if (moreResults) {
         return getAllForkedRepos(page + 1);
       }
